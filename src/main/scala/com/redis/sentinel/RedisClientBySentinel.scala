c727cfbbc3451a9089cf95570a153f5f0e66ce5d
keren <ke@spaceapegames.com>
2014-09-11 09:56:39 +0100

    added redis client by sentinel

---
+++ src/main/scala/com/redis/sentinel/RedisClientBySentinel.scala
@@ -0,0 +1,53 @@
+package com.redis.sentinel
+
+import com.redis.{RedisNode, RedisMasterNotFoundException, RedisClient}
+
+class RedisClientBySentinel(val masterName: String, sentinelCluster: SentinelCluster, onClientConnected: RedisNode => Unit) extends SentinelMonitoredRedisMaster {
+  private var host: String = null
+  private var port: Int = 0
+  private var client: RedisClient = null
+
+  init
+
+  private def init {
+    initMasterNode
+    reconnectClient
+  }
+
+  private def initMasterNode {
+    val redisNode = sentinelCluster.getMasterNode(masterName).getOrElse(throw new RedisMasterNotFoundException(masterName))
+    host = redisNode.host
+    port = redisNode.port
+  }
+
+  def reconnectClient {
+    if (client != null) {
+      client.disconnect
+    }
+    val newClient = new RedisClient(host, port)
+    client = newClient
+  }
+
+  private def updateClient(newRedisNode: RedisNode) {
+    if (newRedisNode.host != host || newRedisNode.port != port) {
+      host = newRedisNode.host
+      port = newRedisNode.port
+      reconnectClient
+      onClientConnected(newRedisNode)
+    }
+  }
+
+  def getMasterName: String = masterName
+
+  def onMasterChange(newRedisNode: RedisNode) {
+    updateClient(newRedisNode)
+  }
+
+  def onMasterHeartBeat(newRedisNode: RedisNode) {
+    updateClient(newRedisNode)
+  }
+
+  def withClient[T](body: RedisClient => T) = {
+    body(client)
+  }
+}
